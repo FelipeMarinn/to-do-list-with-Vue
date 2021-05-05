2ed009cb5ba14d4d5225178d87fe1f8ff7cf0383
felipe <felipeguisao18@gmail.com>
2021-05-05 01:49:40 -0500

    Agrega Proyecto

---
+++ src/components/Tarea.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+    <input class="todo-checkbox" 
+           v-bind:id="'item_' + item.id" 
+           v-model="item.done" type="checkbox">
+    <label v-bind:for="'item_' + item.id"></label>
+    <span class="todo-text">{{ item.title }}</span>
+    <span class="delete" v-on:click.prevent="deleteItem(item)"></span>
+</li>
+</template>
+
+<script>
+export default {
+  name: 'Tarea',
+  props: {
+    todoList:Array,
+    item: Object
+  },
+  methods: {
+    deleteItem:function(item) {
+      this.todoList.splice(this.todoList.indexOf(item), 1);
+    }
+  }
+}
+</script>
